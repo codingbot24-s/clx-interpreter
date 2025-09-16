4e7bb4719a7f9ea07a38c447a479537405c17e12
pcodefromarch <primecoder8@gmail.com>
2025-09-16 21:52:06 +0530

    add reallocate

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,35 +1,44 @@
-
 pub mod chunk {
     enum OPCODE {
-       OPRETURN 
+        OPRETURN,
     }
 
     struct Chunk {
-        code:Box<u8>,
-        count:i32,
-        capacity:i32
+        code: Box<i32>,
+        count: i32,
+        capacity: i32,
     }
-    impl Chunk{
+    impl Chunk {
         fn init_chunk(&mut self) {
-            Chunk{
-                code:Box::new(0),
-                count:0,
-                capacity:0,
+            Chunk {
+                code: Box::new(0),
+                count: 0,
+                capacity: 0,
             };
         }
         fn write_chunk(&mut self) {
+            // is the current array already has the size for new byte if not grow the arr
             if self.count > self.capacity {
-                let old_capacity = self.capacity;
-                self.capacity = grow_capacity(&old_capacity); 
+                // GROW THE CAP
+                self.capacity = grow_capacity(&self.capacity);
+                match reallocate(self.capacity){
+                    Some(new_size) => self.code = new_size,
+                    None => println!("memory not allocated")    
+                }
             }
-        }   
+        }
     }
 
-    pub fn grow_capacity (capacity: &i32) -> i32 {
-        if *capacity < 8 {
-            8
-        }else {
-            capacity * 2
-        }
+    pub fn grow_capacity(capacity: &i32) -> i32 {
+        if *capacity < 8 { 8 } else { capacity * 2 }
+    }
+    // TODO: fn for grow array
+    
+    pub fn reallocate(new_size:i32) -> Option<Box<i32>> {
+        if new_size == 0 {
+            return None;
+        } 
+        let result = Box::new(new_size);       
+        Some(result)
     }
 }
